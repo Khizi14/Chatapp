4660c9587027aa3a42ba8b74a56d2572acf6bf1d
Khizi14 <khizi72@gmail.com>
2022-11-06 00:06:11 +0500

    Home Page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { HomePage } from "./homePage";
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { InputModule } from '../Components/input/input.module';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    InputModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
