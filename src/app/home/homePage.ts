12b1572ce3bf506e760d952839b3b395e86e62b8
Khizi14 <khizi72@gmail.com>
2022-11-06 02:50:11 +0500

    Doctors Grid Update

--- src/app/home/homePage.ts
+++ src/app/home/homePage.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationExtras} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+doctor;
+  constructor(private router: Router) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.doctor = [
+      {
+        img: 'assets/img/Doc1.jpg',
+        name: 'Doctor 1'
+      },
+      {
+        img: 'assets/img/Doc2.jpg',
+        name: 'Doctor 2'
+      },
+      {
+        img: 'assets/img/Doc3.jpg',
+        name: 'Doctor 3'
+      },
+      {
+        img: 'assets/img/Doc4.jpg',
+        name: 'Doctor 4'
+      },
+      {
+        img: 'assets/img/Doc5.jpg',
+        name: 'Doctor 5'
+      }
+      ,
+      {
+        img: 'assets/img/Doc6.jpg',
+        name: 'Doctor 6'
+      }
+    ]
+    
+  }
 
+  goToDoctor(){
+
+  }
 }
 
